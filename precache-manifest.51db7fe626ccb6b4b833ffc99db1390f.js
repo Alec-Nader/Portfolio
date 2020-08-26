1c058f1e0f7333a57a1c493e62a29a0b6b1f9be8
Alec <alecnader@gmail.com>
2020-08-26 07:49:27 +0000

    Updates

---
+++ precache-manifest.51db7fe626ccb6b4b833ffc99db1390f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b54d1f6143b43d399e52d9d7b3183018",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e121842d66be7ca315b0",
+    "url": "/static/css/2.4439b770.chunk.css"
+  },
+  {
+    "revision": "a90e33f7786444865669",
+    "url": "/static/css/main.0845f33a.chunk.css"
+  },
+  {
+    "revision": "e121842d66be7ca315b0",
+    "url": "/static/js/2.98492454.chunk.js"
+  },
+  {
+    "revision": "daa8b543289b2fc25924ba040bf36258",
+    "url": "/static/js/2.98492454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a90e33f7786444865669",
+    "url": "/static/js/main.af963b40.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "dd173dc925a7fd40914c639b1ac2a928",
+    "url": "/static/media/DEP.dd173dc9.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "aa1d88d37a18a031b5a120a23cc9c96c",
+    "url": "/static/media/iMechanic.aa1d88d3.png"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "82d387658cf3b5c6cf6fa3ed892e19cc",
+    "url": "/static/media/nadersfarm.82d38765.png"
+  }
+]);
